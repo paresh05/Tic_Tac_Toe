866c160d6523b670f360bdaeb0f7899c839dbb23
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-08-23 09:36:15 +0530

    Uc1 Initialised array board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,19 +1,19 @@
 package com.bridgelabzworkshop;
 
 public class TicTacToe {
-	
+	//function of user case 1
 	public static void uc1()
 	{
 
 		System.out.println("Welcome to the Tic Tac Toe Game");
-		char arr[][];
-		arr=new char[3][3];
+		char board[][];
+		board=new char[3][3]; 
 		
 		for(int i=0;i<3;i++)
 		{
 			for(int j=0;j<3;j++)
 			{
-				arr[i][j]=0;
+				board[i][j]=' ';
 			}
 		}
 	}
