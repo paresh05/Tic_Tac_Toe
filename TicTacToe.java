1516bc6e7f7c8b0dea7e5120cc650a7ab00ee71f
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-08-24 23:25:53 +0530

    Added a method to make a move

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,16 +4,14 @@ import java.util.Scanner;
 
 public class TicTacToe {
 	
-	public static char board[]=new char[10]; //@param board[] is an array of characters used as a board to play the game 
+	public static char board[]=new char[10]; //@param board[] is an array of characters used as a board to play the game
+	public static char letter; //@param letter is of type character used for user in the game
 	/* Function of createBoard 
 	 * This function creates a board using for loop
 	 * The elements of array board are initialized to empty space */
 	public static void createBoard()
 	{
-
 		System.out.println("Welcome to the Tic Tac Toe Game");
-		
-		
 		for(int i=1;i<board.length;i++)
 		{
 				board[i]=' ';
@@ -26,7 +24,7 @@ public class TicTacToe {
 	{
 		System.out.println("Choose the letter X or O");
 		Scanner sc = new Scanner(System.in);
-		char letter=sc.next().charAt(0);
+		letter=sc.next().charAt(0);
 	}
 	//This function displays the board 
 	public static void showBoard()
@@ -39,11 +37,24 @@ public class TicTacToe {
 		System.out.println("| " + board[7] + " | "+ board[8] + " | " + board[9]+ " |");
 		System.out.println("|---|---|---|");
 	}
+	/* Function makeMove
+	 * This function allows the user to make a move to a desired location in the board by taking input from the user*/
+	public static void makeMove()
+	{
+		System.out.println("Make a move");
+		System.out.println("Select any index between 1 to 9 to make a move");
+		Scanner sc = new Scanner(System.in);
+		int position=sc.nextInt();
+		if(board[position]==' ')
+			board[position]=letter;
+	}
 	public static void main(String[] args) {
 			//Function call
 			createBoard();
-			showBoard();
 			userInput();
+			showBoard();
+			makeMove();
+			showBoard();
 			
 	}
 
