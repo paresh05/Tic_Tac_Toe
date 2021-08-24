0dd57ec5164ee7d7970e7fa030b1a2b99ec6c93d
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-08-24 23:43:00 +0530

    Added a method to check who plays first

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class TicTacToe {
 	
 	public static char board[]=new char[10]; //@param board[] is an array of characters used as a board to play the game
-	public static char letter; //@param letter is of type character used for user in the game
+	public static char userLetter; //@param userLetter is of type character used for user in the game
+	public static char computerLetter; //@param computerLetter is of type character used for computer in the game
 	/* Function of createBoard 
 	 * This function creates a board using for loop
 	 * The elements of array board are initialized to empty space */
@@ -24,7 +25,11 @@ public class TicTacToe {
 	{
 		System.out.println("Choose the letter X or O");
 		Scanner sc = new Scanner(System.in);
-		letter=sc.next().charAt(0);
+		userLetter=sc.next().charAt(0);
+		if(userLetter=='O')
+			computerLetter='X';
+		else
+			computerLetter='O';
 	}
 	//This function displays the board 
 	public static void showBoard()
@@ -48,26 +53,48 @@ public class TicTacToe {
 		while(madeaMove==0) {
 			int position=sc.nextInt();
 			if(board[position]==' ') {
-				board[position]=letter;
+				board[position]=userLetter;
 				madeaMove=1;
 			}
 			else {
 				System.out.println("Not a freespace. Make a different move");
 			}
 		}
+	}
+	/* Function computerMakeMove
+	 * This function allows the computer make move */
+	public static void computerMakeMove()
+	{
+		int index = ((int) Math.floor(Math.random()*10) % 9)+1;
+		board[index]=computerLetter;
+	}
+	/* Function toss
+	 * This function is used to determine Heads or Tails and assign accordingly who starts first, the computer or the user*/
+	public static void toss()
+	{
+		int userToss;
+		System.out.println("Enter 1 or 0 to check who plays first");
+		Scanner sc = new Scanner(System.in);
+		userToss=sc.nextInt();
+
+		int randomToss = (int) Math.floor(Math.random()*10) % 2;
 		
+		if(userToss==randomToss) {
+			System.out.println("You Play First");
+			makeMove();
+			}
+		else {
+			System.out.println("Computer plays first");
+			computerMakeMove();
+		}
+		showBoard();
 	}
 	public static void main(String[] args) {
 			//Function call
 			createBoard();
 			userInput();
 			showBoard();
-			makeMove();
-			showBoard();
-			makeMove();
-			showBoard();
-			makeMove();
-			showBoard();
+			toss();
 			
 	}
 
