1423ea13fa8f263921918a62f8f0f4d239ed5029
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-08-24 23:00:26 +0530

    Added method to allow player to input

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,26 +1,38 @@
 package com.bridgelabzworkshop;
 
+import java.util.Scanner;
+
 public class TicTacToe {
-	//function of user case 1
-	public static void uc1()
+	
+	public static char board[]=new char[10]; //@param board[] is an array of characters used as a board to play the game 
+	/* Function of createBoard 
+	 * This function creates a board using for loop
+	 * The elements of array board are initialized to empty space */
+	public static void createBoard()
 	{
 
 		System.out.println("Welcome to the Tic Tac Toe Game");
-		char board[][];
-		board=new char[3][3]; 
 		
-		for(int i=0;i<3;i++)
+		
+		for(int i=1;i<board.length;i++)
 		{
-			for(int j=0;j<3;j++)
-			{
-				board[i][j]=' ';
-			}
+				board[i]=' ';
 		}
+	}
+	/* Function of user input
+	 * This function takes the input from user 
+	 * It allows player to choose a letter X or O */
+	public static void userInput()
+	{
+		System.out.println("Choose the letter X or O");
+		Scanner sc = new Scanner(System.in);
+		char letter=sc.next().charAt(0);
 	}
 
 	public static void main(String[] args) {
-
-			uc1();
+			//Function call
+			createBoard();
+			userInput();
 	}
 
 }
